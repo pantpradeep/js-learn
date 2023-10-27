63b4906dde0226897e5e2d42f4f054e96ecbbe05
pradeeppant <pradeeppantppp@gmail.com>
2023-10-27 13:18:51 +0530

    update data types

--- 02_data-types.js
+++ 02_data-types.js
@@ -1,3 +1,10 @@
+// js is dynamic type language
+
+const score = 100 //we did not tell the type (so its dynamic type lang)
+
+//primitive and non primitive
+
+//primitive data types are(call by value)
 //number
 //string
 //boolean
@@ -6,7 +13,22 @@
 //symbol
 //bigint
 
+//non-primitive(call by reference type) 
+//objects
+//functions
+//array
+
 let state = null // null means empty
 let accountName; // undefined
 
 console.log(typeof accountName)
+
+
+const heros = ['ironman', 'spiderman', 'locky'];
+const person = {
+    name:'pradeep',
+    age:'32'
+}
+const myFunc = function(){
+    return 'test'
+}
