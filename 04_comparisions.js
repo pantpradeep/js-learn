f3b4af9f3562220768f00a8f278628ac393017e9
pradeeppant <pradeeppantppp@gmail.com>
2023-10-27 13:00:54 +0530

    opertions

---
+++ 04_comparisions.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2">1); // convert string into number
+console.log("02">1)
+
+//strict check, check value and data type as well
+
+console.log("with ==","2"== 2); // convert string into number thats why true
+
+console.log("with ===", "2"===2); //false
